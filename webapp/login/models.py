5ffc0b6da2bcfe2c897e2341244f3d08d29e2026
Kartel1 <loick.lutin@ensam.eu>
2016-11-30 16:27:40 +0100

    ajout d'un formulaire

--- webapp/login/models.py
+++ webapp/login/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,6 +9,11 @@ class Usager(models.Model):
     password = models.CharField(max_length=100)
     user_name = models.CharField(max_length=250)
     user_logo = models.CharField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('login:connexion')
+        #kwargs={'pk': self.pk}
+
 
     def __str__(self):
         return self.mail + ' - ' + self.user_name
