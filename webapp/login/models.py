42699de89f2d4c850a078d2938b79abc96797d52
Kartel1 <loick.lutin@ensam.eu>
2017-01-04 08:20:11 +0100

    Mise en forme propre des urls

--- webapp/login/models.py
+++ webapp/login/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Personne(models.Model):
     usager = models.ForeignKey(User, on_delete = models.CASCADE)
     user_infos = models.CharField(max_length=1000)
     user_logo = models.FileField()
-    #slug = models.SlugField(unique=True)
-
-    #def get_absolute_url(self):
-        #return reverse('login:detail', kwargs={'pk': self.pk})
-
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return str(self.usager)
+
+    def get_absolute_url(self):
+        return reverse('login:detail', kwargs={'slug': self.slug})
+
+
 
 class Doc(models.Model):
     utilisateur = models.ForeignKey(Personne, on_delete = models.CASCADE)
@@ -26,7 +25,7 @@ class Doc(models.Model):
     is_favorite = models.BooleanField(default=False)
 
     def get_absolute_url(self):
-        return reverse('login:detail', kwargs={'pk': self.utilisateur.pk})
+        return reverse('login:detail', kwargs={'slug': self.utilisateur.slug})
 
     def __str__(self):
         return  self.fichier_titre
